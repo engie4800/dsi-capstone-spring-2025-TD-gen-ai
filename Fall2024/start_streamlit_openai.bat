0d7973eb4c327a6a4e17e4a4903429026ae48985
samgabor <81497133+samgabor@users.noreply.github.com>
2025-03-03 08:50:57 -0500

    Added support for 1536 embedding
    Created a new index that mimics last team's document loads so we can use OpenAI LLMs

--- Fall2024/start_streamlit_openai.bat
+++ Fall2024/start_streamlit_openai.bat
@@ -1,6 +1,6 @@
 set backend=openai
 set llm_model_name=gpt-4o
 set embed_model_name=text-embedding-ada-002
-set pine_index_name=td-bank-docs
+set pine_index_name=td-bank-docs-openai
 
 streamlit run chatbot.py
--- Fall2024/start_streamlit_openai.sh
+++ Fall2024/start_streamlit_openai.sh
@@ -1,6 +1,6 @@
 export backend="openai"
 export llm_model_name="gpt-4o"
 export embed_model_name="text-embedding-ada-002"
-export pine_index_name="td-bank-docs"
+export pine_index_name="td-bank-docs-openai"
 
 streamlit run chatbot.py

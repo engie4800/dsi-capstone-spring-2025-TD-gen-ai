dd970391c126a9780502c3dd0fbf1d3a8a956617
samgabor <81497133+samgabor@users.noreply.github.com>
2025-03-08 14:12:36 -0500

    Defaulted use_cohere to True in shell files

--- Fall2024/start_streamlit_ollama.bat
+++ Fall2024/start_streamlit_ollama.bat
@@ -2,6 +2,6 @@ set backend=ollama
 set llm_model_name=mistral
 set embed_model_name=nomic-embed-text
 set pine_index_name=td-bank-docs-new
-set use_cohere=False
+set use_cohere=True
 
 streamlit run chatbot.py
--- Fall2024/start_streamlit_openai.bat
+++ Fall2024/start_streamlit_openai.bat
@@ -2,6 +2,6 @@ set backend=openai
 set llm_model_name=gpt-4o
 set embed_model_name=text-embedding-ada-002
 set pine_index_name=td-bank-docs-openai
-set use_cohere=False
+set use_cohere=True
 
 streamlit run chatbot.py

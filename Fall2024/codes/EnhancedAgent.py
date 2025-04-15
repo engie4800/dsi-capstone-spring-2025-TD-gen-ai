c4002f5db35df9caee15a6dfa74a36354e407657
briannata <brianna.hy.ta@gmail.com>
2025-04-15 16:19:25 -0400

    Remove fall2024 folder

--- Fall2024/codes/EnhancedAgent.py
+++
@@ -1,9 +0,0 @@
-#from codes.EnhancedResponseChain import EnhancedResponseChain #this import is using the original enhancedresponsechain instead of the cohere one
-from codes.EnhancedResponseChain_CohereRerank import EnhancedResponseChain #this uses cohere rerank
-
-class EnhancedAgent:
-    def __init__(self, secrets):
-        self.retriever = EnhancedResponseChain(secrets)
-
-    def invoke(self, query, conversation_history=""):
-        return self.retriever.invoke(query, conversation_history)
